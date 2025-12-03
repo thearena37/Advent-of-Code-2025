1b116b2e84349105b001ce827efb07c1b03a68fe
John Arena <john.arena1o@gmail.com>
2025-12-03 10:03:33 -0500

    create day3 directory file and work on pseudocode for part1

---
+++ day3/day3part1.js
@@ -0,0 +1,12 @@
+// Obj: Find the greatest 2 digit combo of numbers, without rearranging the string, and add them together for the total 
+// joltage output
+
+// get input from file
+// convert input to array
+// for each element:
+    // identify the largest number in the file
+    // identify the second largest number in the file
+        // ! can only look after the largest number's position
+    // put the two digits together
+    // convert the string to a number
+    // add number to joltage sum
