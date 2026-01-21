3638ab8dd0aa06d7f428cdcea325bba4c8c17276
John Arena <john.arena1o@gmail.com>
2026-01-21 18:21:15 -0500

    refractor day 3 part 1

--- day3/day3part1-solved.js
+++ day3/day3part1-solved.js
@@ -2,60 +2,38 @@
 // and add them together for the total joltage output
 const fs = require("fs");
 
-// returns the largest number in the
-function findLargest(battArray) {
-    // let largest = {}
-    // create a new array and sort
-    let sorted = battArray.map((x) => x);
-    sorted.sort((a, b) => b - a);
-    let loop = -1;
-
-    // log the largest digit in the array
-    let largest = {
-        num: sorted[0],
-        pos: battArray.indexOf(sorted[0]),
-    };
-    // if the largest number in the array is the last digit, pick the next largest digit
-    if (battArray.indexOf(sorted[0]) == battArray.length - 1) {
-        largest = {
-            num: sorted[1],
-            pos: battArray.indexOf(sorted[1]),
-        };
-    }
-
-    // find the second largest number in the remaining string after the largest number
-    let secondLargest = battArray
-        .slice(largest.pos + 1)
-        .sort((a, b) => b - a)[0];
-
-    console.log(largest.num, secondLargest)
-
-    return Number(largest.num.toString() + secondLargest.toString());
-}
-
-// get input from file
+// get input from file and define variables
 const file = fs.readFileSync(
     `${__dirname}/../inputs/day3input-test.json`,
-    "utf8"
+    "utf8",
 );
 const data = JSON.parse(file);
-
-console.log('data', data)
-// convert input to array
-// const batteryBanks = data.batteryBanksTest;
 const batteryBanks = data.batteryBanks;
-
-
 let joltageSum = 0;
 
 // for each element:
 batteryBanks.forEach((bank) => {
     // convert bank into array of strings
-    console.log(bank)
-    let batteries = bank.toString().split("");
-    // console.log(batteries)
+    let battery = bank.toString().split("");
 
-    joltageSum += findLargest(batteries);
+    // create a new array and sort
+    let sorted = battery.map((x) => x);
+    sorted.sort((a, b) => b - a);
+
+    // if the largest number is the last digit in battery, return 1
+    let element = battery.indexOf(sorted[0]) == battery.length - 1 ? 1 : 0;
+
+    // select largest number / position based on element variable
+    let largest = {
+        num: sorted[element],
+        pos: battery.indexOf(sorted[element]),
+    };
+
+    // find the second largest number in the remaining string after the largest number
+    let secondLargest = battery.slice(largest.pos + 1).sort((a, b) => b - a)[0];
+
+    // add number to joltageSum
+    joltageSum += Number(largest.num.toString() + secondLargest.toString());
 });
 
 console.log("Joltage Sum:", joltageSum);
