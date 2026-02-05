5e874d784f5a328d9266e84bc71d22b502232a18
John Arena <john.arena1o@gmail.com>
2026-02-05 18:08:45 -0500

    finish day 4 part 1

--- day4/day4part1.js
+++ day4/day4part1.js
@@ -1,71 +1,58 @@
 // Objective: find the rolls of paper that can be accessed by
-//   a forklift. (of the 8 adjecent tiles to a given paper roll,
-//   four of them must be empty)
+//   a forklift. (of the 8 adjacent tiles to a given paper roll,
+//   at least 5 of them must be empty)
 const fs = require("fs");
 
-const file = fs.readFileSync(
-    `${__dirname}/../inputs/day4inputs.json`,
-    "utf8",
-);
+// get puzzle input
+const file = fs.readFileSync(`${__dirname}/../inputs/day4inputs.json`, "utf8");
 const data = JSON.parse(file);
-const paperRolls = data.paperRollsTest
+const paperRolls = data.paperRolls;
 
-// data needs to be converted into a 2D grid
-// move through each item
-// skip items that are '.'
-// look at each adjacent position for '@'s
-// add to a valid counter if one of the adjacent positons is '.'
-// also add to a counter if one of the adjacent positons 
-//   doesn't exist
-// if there are 5 or more empty positions around it, then turn
-//   the '@' to an 'X'
+// create 2-D array
+floorMap = paperRolls.map((roll) => roll.split(""));
 
-// when checking position, avoid `-1` as a math result,
-//   because it will just wrap the element to the back
-//   of the array and not accurately record the empty
-//   position
+function checkAdjacents(row, col) {
+    const rowOffset = [-1, 0, 1];
+    const colOffset = [-1, 0, 1];
+    let emptySpaces = 0;
 
-// where you need to look in relation to position
-    // pos(col-1)(row-1)
-    // pos(col-1)
-    // pos(col-1)(row+1)
-
-    // pos(col)(row-1)
-    // pos(col)(row+1)
-
-    // pos(col+1)(row-1)
-    // pos(col+1)
-    // pos(col+1)(row+1)
-
-paperRolls.forEach(roll => {
-    roll = roll.split("")
-    console.log(roll)
-});
-
-for (let row = 0; row < paperRolls.length; row++) {
-    for (let col = 0; col < paperRolls[0].length; col++) {
-        let counter = 0
-        let currPos = paperRolls[row][col]
-        
-        while (counter < 5) {
-            if (paperRolls[row-1][col-1] == '.') { counter += 1 }
-            if (paperRolls[row-1][col] == '.') { counter += 1 }
-            if (paperRolls[row-1][col+1] == '.') { counter += 1 }
-    
-            if (paperRolls[row][col-1] == '.') { counter += 1 }
-            if (paperRolls[row][col+1] == '.') { counter += 1 }
-    
-            if (paperRolls[row+1][col-1] == '.') { counter += 1 }
-            if (paperRolls[row+1][col] == '.') { counter += 1 }
-            if (paperRolls[row+1][col+1] == '.') { counter += 1 }
+    // check every offset (both for loops)
+    for (rOff of rowOffset) {
+        for (cOff of colOffset) {
+            // not adjacent, skip
+            if (rOff === 0 && cOff === 0) { continue; }
+            
+            // if the adjacent coordinate if off the map or the space is empty, increment emptySpaces
+            if (
+                paperRolls[row + rOff]?.[col + cOff] === undefined ||
+                paperRolls[row + rOff][col + cOff] === "."
+            ) { emptySpaces += 1; }
         }
-
-        if (counter > 4) {
-            currPos = 'x'
-        }
-        
     }
-    
+    return emptySpaces;
 }
 
-console.log(paperRolls[3][4])
+function checkCoordinates() {
+    let movableRolls = 0;
+
+    // check every coordinate (both for loops)
+    for (let row = 0; row < floorMap.length; row++) {
+        for (let col = 0; col < floorMap[row].length; col++) {
+            // skip coordinates that are already empty
+            if (floorMap[row][col] === ".") { continue; }
+
+            // check each adjacent
+            let emptyAdjacents = checkAdjacents(row, col);
+
+            // if there are more than 4 empty cells, increment movableRolls
+            if (emptyAdjacents > 4) {
+                movableRolls += 1;
+                floorMap[row][col] = "X";
+            }
+        }
+    }
+
+    console.log(movableRolls);
+}
+
+checkCoordinates();
